0e66a219fb36effc73759d0b10c7d2f1209b65a3
Turobjon <avezovzamir337@gmail.com>
2024-04-08 13:18:03 +0500

    turobjon

---
+++ js.js
@@ -0,0 +1,15 @@
+let ism = prompt(`String  typedagi malumotn kiriting  `)
+
+let number = prompt(`number  typedagi malumotn kiriting  `)
+
+let boolean = prompt(` boolean  typedagi malumotn kiriting  `)
+
+
+boolean =confirm( `hamma malumotlarni togri kiritganingizga ishonchingiz komilmi`)
+
+
+alert( `siz kiritgan  ${ism} sozi string typega tegishli`  )
+
+alert( `siz kiritgan  ${number} sozi number typega tegishli`)
+
+alert( `siz kiritgan  ${boolean} sozi boolean typega tegishli`)
